42dd2e7832468c060bf0014f7f97ad79d2e68ee5
Alexandru Calugari <afishr00@gmail.com>
2021-03-03 14:26:06 +0200

    chore(test): Remove tests

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule Rtp.MixProject do
+defmodule RTP.MixProject do
   use Mix.Project
 
   def project do
--- test/rtp_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RtpTest do
-  use ExUnit.Case
-  doctest Rtp
-
-  test "greets the world" do
-    assert Rtp.hello() == :world
-  end
-end
