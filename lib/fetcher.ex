c2591b426a400edc2807cd391a03ade7823685a9
Alexandru Calugari <afishr00@gmail.com>
2021-03-04 22:41:44 +0200

    chore(): Format code

--- lib/fetcher.ex
+++ lib/fetcher.ex
@@ -1,15 +1,14 @@
 defmodule Fetcher do
   def fetch() do
-      receive do
-          tweet ->
-              Manager.manage(tweet)
-              fetch()
-      end
+    receive do
+      tweet ->
+        Manager.manage(tweet)
+        fetch()
+    end
   end
 
   def init() do
-      EventsourceEx.new("localhost:4000/tweets/1", stream_to: self())
-      fetch()
+    EventsourceEx.new("localhost:4000/tweets/1", stream_to: self())
+    fetch()
   end
-
 end
--- lib/printer.ex
+++ lib/printer.ex
@@ -2,23 +2,21 @@ defmodule Printer do
   use GenServer
 
   def start(index) do
-      GenServer.start_link(__MODULE__, :ok, name: {:global, index})
+    GenServer.start_link(__MODULE__, :ok, name: {:global, index})
   end
 
   def print(tweet) do
-      GenServer.cast(__MODULE__, {:print, tweet})
+    GenServer.cast(__MODULE__, {:print, tweet})
   end
-
 
   @impl true
   def init(:ok) do
-      {:ok, %{}}
+    {:ok, %{}}
   end
 
   @impl true
   def handle_cast({:print, tweet}, _) do
-      IO.inspect(self())
-      {:noreply, IO.puts(tweet)}
+    IO.inspect(self())
+    {:noreply, IO.puts(tweet)}
   end
-
 end
