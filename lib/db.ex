f0be3717ac3f5bf6cc4baabe27a531c4092acfac
Alexandru Calugari <afishr00@gmail.com>
2021-04-27 20:06:01 +0300

    feat(db): Add adaptive batching for savign in db

--- lib/db.ex
+++ lib/db.ex
@@ -4,7 +4,11 @@ defmodule DB do
   def start() do
     {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:27013", database: "rtp")
 
-    GenServer.start_link(__MODULE__, %{connection: conn, tweets: [], users: []}, name: __MODULE__)
+    GenServer.start_link(
+      __MODULE__,
+      %{connection: conn, tweets: [], users: [], time: :os.system_time(:millisecond)},
+      name: __MODULE__
+    )
   end
 
   @impl true
@@ -18,8 +22,9 @@ defmodule DB do
 
   @impl true
   def handle_cast({:save_one, tweet, user}, state) do
-    IO.inspect(tweet)
-    if length(state.tweets) == 128 do
+    now = :os.system_time(:millisecond)
+
+    if length(state.tweets) == 128 or now - state.time > 200 do
       Mongo.insert_many(state.connection, "tweets", state.tweets)
       Mongo.insert_many(state.connection, "users", state.users)
 
@@ -27,14 +32,16 @@ defmodule DB do
        %{
          connection: state.connection,
          tweets: [],
-         users: []
+         users: [],
+         time: :os.system_time(:millisecond)
        }}
     else
       {:noreply,
        %{
          connection: state.connection,
          tweets: [tweet | state.tweets],
-         users: [user | state.users]
+         users: [user | state.users],
+         time: state.time
        }}
     end
   end
